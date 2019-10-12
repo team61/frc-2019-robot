08a945279106ed635498c89f1e33438f88b2f350
Thomas Buckley <tbuckley@tabwebsites.com>
2019-10-12 11:37:55 -0400

    added documentation to MoveToTarget class

--- src/main/java/frc/robot/commands/Drivetrain/MoveToTarget.java
+++ src/main/java/frc/robot/commands/Drivetrain/MoveToTarget.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Drivetrain;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
@@ -13,7 +14,7 @@ public class MoveToTarget extends Command {
 
     private PIDController TurnPID;
     private PIDController SlowDownPID;
-    private double turnSpeed;
+    public double turnSpeed;
     private double driveSpeed;
     private final double slowdownSensitivity = 2;
     private final int CENTER = 158;
@@ -23,6 +24,14 @@ public class MoveToTarget extends Command {
         requires(visioncam);
         requires(drivetrain);
 
+        /**
+         * Turn PID implementation - calculates error of turning
+         * using filtered vision camera data as imput for the PID
+         * loop. PIDOutput (the output of the PID loop) is assigned
+         * to turnSpeed double. Uses lambda to replace anonymous
+         * override of the PIDOutput() abstract class, which solely
+         * assigns the output of the PIDController loop to an object.
+         */
         TurnPID = new PIDController(0.00633, 0, 0, new PIDSource() {
 
             PIDSourceType pidSource;
@@ -39,6 +48,10 @@ public class MoveToTarget extends Command {
                 return CENTER - visioncam.getPixy().getX();
             }
         }, output -> turnSpeed = output);
+
+        /**
+         * Drivespeed PID implementation - refer to above documentation.
+         */
         SlowDownPID = new PIDController(0.0714, 0, 0, new PIDSource() {
 
             PIDSourceType pidSource;
@@ -56,6 +69,7 @@ public class MoveToTarget extends Command {
             }
         }, output -> driveSpeed = output/(output + slowdownSensitivity));
 
+        // Setting Absolute Values of PID Controllers
         TurnPID.setAbsoluteTolerance(0.05);
         SlowDownPID.setAbsoluteTolerance(0.05);
 
@@ -77,6 +91,7 @@ public class MoveToTarget extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        // drives robot based on PID values calculated from analysis of vision cam
         double speedLeft = driveSpeed + turnSpeed;
         double speedRight = driveSpeed - turnSpeed;
         drivetrain.tankDrive(speedLeft, speedRight);
